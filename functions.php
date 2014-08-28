03cf943a1d7133ea89529e9b3aed936d704e2412
hvaras <king.cold@gmail.com>
2014-08-28 11:24:12 -0400

    se pone proteccion de email

--- functions.php
+++ functions.php
@@ -13,34 +13,36 @@ $email =  $_POST['email'];
 $pregunta = $_POST['pregunta'];
 $respuesta = $_POST['respuesta'];
 
+if($email){
+    if($pregunta==1){
+        $sql = "INSERT INTO trivia (email,r1) VALUES (:email,:r1)";
+        $q = $conn->prepare($sql);
+        $q->execute(array(':email'=>$email,
+            ':r1'=>$respuesta));
 
-if($pregunta==1){
-    $sql = "INSERT INTO trivia (email,r1) VALUES (:email,:r1)";
-    $q = $conn->prepare($sql);
-    $q->execute(array(':email'=>$email,
-        ':r1'=>$respuesta));
-
-    $ultimo = $conn->lastInsertId('id');
-
-
-    $_SESSION['current_user']  = $ultimo;
+        $ultimo = $conn->lastInsertId('id');
 
 
-}else{
-
-    $current_user = $_SESSION['current_user'];
-
-    $sql= "SELECT * FROM trivia WHERE id = $current_user";
-    $stmt = $conn->query($sql);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $_SESSION['current_user']  = $ultimo;
 
 
+    }else{
+
+        $current_user = $_SESSION['current_user'];
+
+        $sql= "SELECT * FROM trivia WHERE id = $current_user";
+        $stmt = $conn->query($sql);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
-    $sql = "UPDATE trivia SET r$pregunta=? WHERE id=?";
-    $q = $conn->prepare($sql);
-    $q->execute(array($respuesta,$row['id']));
+
+
+        $sql = "UPDATE trivia SET r$pregunta=? WHERE id=?";
+        $q = $conn->prepare($sql);
+        $q->execute(array($respuesta,$row['id']));
+    }
 }
+
 
 
 
